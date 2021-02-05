331c953cf1df9c5eebaa02f94170a672c2eb1c3e
Dan Lubbers <lubbers.dan@gmail.com>
2021-02-04 22:31:47 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [value, setValue] = useState(0);
+  const [grids, setGrids] = useState([]);
+
+  const createGrids = (value) => {
+    setGrids([]);
+    for (let i = 1; i <= value; i++) {
+      setGrids((grids) => [...grids, i]);
+    }
+  };
+
+  const renderGrids = grids.map((e, i) => {
+    return (
+      <div className="grid" key={i}>
+        Grid #{e}
+      </div>
+    );
+  });
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+
+      <section>
+        <div>
+          <input type="number" onChange={(e) => setValue(e.target.value)} />
+          <button onClick={() => createGrids(value)}>Populate Grids</button>
+        </div>
+        <div className="gridContainer">{renderGrids}</div>
+      </section>
     </div>
   );
 }
