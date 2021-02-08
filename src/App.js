8506c5f377bc129af6591a8c9d7e4a415bb5e241
Dan Lubbers <lubbers.dan@gmail.com>
2021-02-08 18:57:29 -0500

    Added conditional to account for the user inputing negative numbers

--- src/App.js
+++ src/App.js
@@ -13,6 +13,11 @@ function App() {
     // clear the grids for additional user input
     setGrids([]);
 
+    if (Math.sign(value) !== 1)
+      alert(
+        `${value} is a negative number. Please input a positive number to populate grids!`
+      );
+
     // loop to the value of user input. 'i' value starts at 1 instead of 0 because our grid starts at 1, instead where the array index starts at 0
     for (let i = 1; i <= value; i++) {
       // callback function needed to get the previous grids state, then spread previous state and add the value of i ( increment )
@@ -28,7 +33,10 @@ function App() {
 
       <section>
         <form onSubmit={handleSubmit}>
-          <input type="number" onChange={(e) => setValue(e.target.value)} />
+          <input
+            type="number"
+            onChange={(e) => setValue(parseInt(e.target.value))}
+          />
           <button>Populate Grids</button>
         </form>
         <div className="gridContainer">
